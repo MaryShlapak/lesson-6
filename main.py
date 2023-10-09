cf3c997ab4b2bc877bfadde1798f2984453d5fe1
marys <shlapakmv@gmail.com>
2023-10-10 00:53:41 +0300

    complete task 6.3

--- main.py
+++ main.py
@@ -15,25 +15,29 @@ print(numbers)
 
 def prime_num (number):
     if number <= 1:
-        return  False
-    if number >= 3:
-        return True
+        return False
+    if number <= 3:
+        return  True
     if number % 2 == 0 or number % 3 == 0:
         return False
     num = 5
     while num * num <= number:
         if number % num == 0 or number % (num + 2) == 0:
-        return False
-            num += 6
+            return False
+        num += 6
     return True
 
-def prime_num_sum (numbers):
-    prime = []
+
+def prime_num_count (numbers):
+    prime_count = 0
     for number in numbers:
         if prime_num(number):
-            prime.append(number)
-    return prime
+            prime_count += 1
+    return prime_count
 
+result = prime_num_count(numbers)
 
-result = prime_num_sum(numbers)
 print("The number of prime numbers in list:", result)
+
+
+# З перевіркою на просте число підказали, хотілося б по можливості розібрати це на лекції
